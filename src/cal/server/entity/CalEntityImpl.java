459234af8ef9f40c53fe1bdf7cb67efe6b42ef0b
oemmao <sflower0121@gmail.com>
2017-04-03 11:54:42 +0900

    test_학원
    test_학원

--- src/cal/server/entity/CalEntityImpl.java
+++ src/cal/server/entity/CalEntityImpl.java
@@ -14,6 +14,7 @@ public class CalEntityImpl implements ICalEntity {
 	private PreparedStatement stmt;
 	private Statement stmt1;
 	private ResultSet rs;
+	private List<String[]> resultList;
 	
 	public CalEntityImpl() {
 		exceptionMsg = new Properties(); //예외메시지 파일 불러오기~
@@ -28,45 +29,24 @@ public class CalEntityImpl implements ICalEntity {
 	}
 	
 	public List<String[]> doService(CalVO[] cals) {	
-		List<String[]> resultList = new ArrayList<String[]>();		
+		resultList = new ArrayList<String[]>();		
 		try {
 			con = DriverManager.getConnection(url,user,pass); //JDBC 2.커넥션 획득 //공유가능
 			String query = "insert into tb_cal(id, op1, op, op2, result)" +
 					"values(seq_log.nextval, ?, ?, ?, ?)";
-			stmt = con.prepareStatement(query);	//JDBC 3.Statement 객체 생성
+			stmt = con.prepareStatement(query);	//JDBC 3.Statement 객체 생성 //JDBC 4.쿼리전송
 			for (int i=0; i < cals.length; i++) {							
-				 stmt.setInt(1, Integer.parseInt(cals[i].getOp1()));
-				 stmt.setString(2, cals[i].getOp());
-				 stmt.setInt(3, Integer.parseInt(cals[i].getOp2()));
-				 stmt.setInt(4, cals[i].getResult());
-				//JDBC 4.쿼리전송
+				stmt.setInt(1, Integer.parseInt(cals[i].getOp1()));
+				stmt.setString(2, cals[i].getOp());
+				stmt.setInt(3, Integer.parseInt(cals[i].getOp2()));
+				stmt.setInt(4, cals[i].getResult());
+				
 				int j = stmt.executeUpdate(); //응답을 필요로 하지 않음.		
 				System.out.println(j+"행이 추가되었습니다.");
 				
 				if (j > 0) {
-					stmt1 = con.createStatement();
-					String query1 = "select * from (select * from tb_cal order by id desc) where rownum <2"; //result값을 가져오기
-					
-					rs = stmt1.executeQuery(query1);
-					rs.next();
-					String id = rs.getString("id").trim();
-					String op1 = rs.getString("op1").trim();
-					String op = rs.getString("op").trim();
-					String op2 = rs.getString("op2").trim();
-					String result = rs.getString("result").trim();	
-										
-					String[] ops = new String[5];
-					ops[0] = id;
-					ops[1] = op1;
-					ops[2] = op;
-					ops[3] = op2;
-					ops[4] = result;
-					
-					resultList.add(ops);
+					tableData();
 				}
-//					query = "insert into tb_msg(id, code, message)" +
-//							"values(msg_log.nextval,'100','AddZeroException')" ;
-//					int k = stmt.executeUpdate(query);
 			}	  
 		} catch (SQLException e) {
 				e.printStackTrace();
@@ -103,6 +83,32 @@ public class CalEntityImpl implements ICalEntity {
 	return resultList;
 	}
 
+	public void tableData() {
+		try {
+			stmt1 = con.createStatement();
+			String query1 = "select * from (select * from tb_cal order by id desc) where rownum <2"; //result값을 가져오기
+			
+			rs = stmt1.executeQuery(query1);
+			rs.next();
+			String id = rs.getString("id").trim();
+			String op1 = rs.getString("op1").trim();
+			String op = rs.getString("op").trim();
+			String op2 = rs.getString("op2").trim();
+			String result = rs.getString("result").trim();	
+								
+			String[] ops = new String[5];
+			ops[0] = id;
+			ops[1] = op1;
+			ops[2] = op;
+			ops[3] = op2;
+			ops[4] = result;
+			
+			resultList.add(ops);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public String getMsgAddZeroExc() {	
 		return exceptionMsg.getProperty("e1");
 	}
